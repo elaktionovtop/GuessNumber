b950d17659801963a84f0175be578990bd2c5b4f
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-05 15:40:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+вывети заголовок программы
+
+выйти из программы
+*/
+
+WriteTitle("Угадай число!");
+ExitApp();
+
+void WriteTitle(string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine(new string('-', title.Length));
+    Console.WriteLine();
+}
+
+void ExitApp()
+{
+    Console.WriteLine();
+    Console.Write("Для выхода нажми Enter");
+    Console.ReadLine();
+}
